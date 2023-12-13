c90f38163dee4b1a5b2f0d9a0f16d3fff8db8ce3
RamitVishwakarma <vishwakarmaramit@gmail.com>
2023-12-13 23:26:47 +0530

    Latest changes
    New changes

--- Backend/index.js
+++ Backend/index.js
@@ -5,16 +5,34 @@ const dotenv = require("dotenv").config({
   path: path.resolve(__dirname, "../.env"),
 });
 const mongoose = require("mongoose");
-const initialposts = require("../Assets/blog_posts");
-// const { title } = require("process");
-// const exp = require("constants");
+// const initialposts = require("../Assets/blog_posts");
+// const { nextTick } = require("process");
+
+//using cloudinary to handle image uploads
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "Blog-posts",
+    allowedFormats: ["png", "jpg", "jpeg"],
+  },
+});
+const upload = multer({ storage: storage });
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
-    console.log("DB connected");
+    console.log("Database connected successfully");
   })
   .catch((err) => {
     console.log("Error in connecting to DB", err);
@@ -22,7 +40,7 @@ mongoose
 //Creating schema
 const blogSchema = new mongoose.Schema({
   author: String,
-  password: String,
+  // password: String,
   image: String,
   title: String,
   content: { para1: String, para2: String, para3: String },
@@ -30,17 +48,70 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-//setting views
-app.set("view engine", "ejs");
+app.set("view engine", "ejs"); //setting views
 app.set("views", path.join(__dirname, "../Frontend/views"));
 app.use(express.static(path.join(__dirname, "../Frontend/public")));
 
+// Home Route
 app.get("/", async (req, res) => {
-  //added the initial posts basically with this
-  // await Blog.insertMany(initialposts);
+  //added the initial posts with await Blog.insertMany(initialposts);
   const blogs = await Blog.find({});
-  res.render("frontpage", { blogs });
+  res.render("frontPage", { blogs });
 });
 
+// Write Route
+app.get("/posts", async (req, res) => {
+  res.render("newPost");
+});
+
+// Write Form Route
+app.post("/posts", upload.single("listingImage"), async (req, res) => {
+  // const formData = await req.FormData();
+  // console.log(JSON.stringify(req.file));
+  console.log(req);
+  // console.log(req.body);
+  // console.dir(req.file, { depth: null });
+  const blog = new Blog({
+    author: req.body.author,
+    image: req.file.path,
+    title: req.body.title,
+    content: {
+      para1: req.body.para1,
+      para2: req.body.para2,
+      para3: req.body.para3,
+    },
+  });
+  try {
+    await blog.save();
+    res.json(blog); // Send the blog object as JSON
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error saving the blog post");
+  }
+});
+
+// Read Route
+app.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    // Validate MongoDB ObjectID format
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).render("404");
+    }
+    const post = await Blog.findById(id);
+    if (!post) {
+      return res.status(404).render("404");
+    }
+    res.render("blogPosts", { post });
+  } catch (error) {
+    console.error(error); // Log the actual error for debugging
+    res.status(500).send("Server Error");
+  }
+});
+
+//Error 404 page
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 //listening at port 3000
 app.listen(3000);
